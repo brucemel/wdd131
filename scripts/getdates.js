20686e491983a9a2c35a1debe10f1577944294cf
brucemel <mel24004@byui.edu>
2025-03-10 21:07:40 -0500

    newdate

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.getElementById("lastModified")
+
+// use the data object
+const today = new Date();
+
+currentyear.innerHTML = `${today.getFullYear()}`;
+lastModified.innerHTML = `Last Modified ${document.lastModified}`;
+
