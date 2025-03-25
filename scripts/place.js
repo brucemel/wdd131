47b78c46b991a80fd7aa36df60e6f3a183703f71
brucemel <mel24004@byui.edu>
2025-03-25 18:36:32 -0500

    compudisplay

---
+++ scripts/place.js
@@ -0,0 +1,8 @@
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.getElementById("lastModified")
+
+// use the data object
+const today = new Date();
+
+currentyear.innerHTML = `${today.getFullYear()}`;
+lastModified.innerHTML = `Last Modified ${document.lastModified}`;
