10e9f8ea2e4788615a1fa0fd7a8820ce2ea3ddd2
brucemel <mel24004@byui.edu>
2025-04-10 18:29:02 -0500

    formprofi1

--- scripts/form.js
+++ scripts/form.js
@@ -34,4 +34,12 @@ const products = [
     option.textContent = product.name;
     selectElement.appendChild(option);
   });
-  
+
+  const currentyear = document.querySelector("#currentyear");
+const lastModified = document.getElementById("lastModified")
+
+// use the data object
+const today = new Date();
+
+currentyear.innerHTML = `${today.getFullYear()}`;
+lastModified.innerHTML = `Last Modified: ${document.lastModified}`;
